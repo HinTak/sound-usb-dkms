ad464a29a2d088057cce7f9b378307e8d7155143
Vladimir Sadovnikov <sadko4u@gmail.com>
2020-10-12 12:59:32 +0300

    Added patch related to 3rd gen Focusrite Scarlett devices: https://linuxmusicians.com/viewtopic.php?p=122852#p122852
    Signed-off-by: Hin-Tak Leung <htl10@users.sourceforge.net>

--- sound/usb/pcm.c
+++ sound/usb/pcm.c
@@ -379,6 +379,13 @@ static int set_sync_ep_implicit_fb_quirk(struct snd_usb_substream *subs,
 		ep = 0x82;
 		ifnum = 0;
 		goto add_sync_ep_from_ifnum;
+	case USB_ID(0x1235, 0x8212): /* Focusrite Scarlett 4i4 3rd Gen */
+	case USB_ID(0x1235, 0x8213): /* Focusrite Scarlett 8i6 3rd Gen */
+	case USB_ID(0x1235, 0x8214): /* Focusrite Scarlett 18i8 3rd Gen */
+	case USB_ID(0x1235, 0x8215): /* Focusrite Scarlett 18i20 3rd Gen */
+		ep = 0x81;
+		ifnum = 2;
+		goto add_sync_ep_from_ifnum;
 	case USB_ID(0x0582, 0x01d8): /* BOSS Katana */
 		/* BOSS Katana amplifiers do not need quirks */
 		return 0;
@@ -401,10 +408,11 @@ static int set_sync_ep_implicit_fb_quirk(struct snd_usb_substream *subs,
 add_sync_ep_from_ifnum:
 	iface = usb_ifnum_to_if(dev, ifnum);
 
-	if (!iface || iface->num_altsetting < 2)
-		return -EINVAL;
+	if (!iface || iface->num_altsetting < 2 ||
+	    altsd->bAlternateSetting >= iface->num_altsetting)
+	return -EINVAL;
 
-	alts = &iface->altsetting[1];
+	alts = usb_altnum_to_altsetting(iface, altsd->bAlternateSetting);
 
 add_sync_ep:
 	subs->sync_endpoint = snd_usb_add_endpoint(subs->stream->chip,
